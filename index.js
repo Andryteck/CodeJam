b1a805d3f52d6d45bd1d76137a4eb854a00043c8
Andryteck <772316andrew@gmail.com>
2019-11-06 23:44:20 +0300

    feat: add basic function,

--- index.js
+++ index.js
@@ -1,38 +1,104 @@
-const firstRow = [
-    [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61],
-    [126, 33, 64, 35, 36, 37, 94, 38, 42, 40, 41, 95, 43]
-];
-const secondRow = [
-    [113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92],
-    [81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 123, 125, 124]
-];
-const thirdRow = [
-    [97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39],
-    [65, 83, 68, 70, 71, 72, 74, 75, 76, 58, 34]
-];
-const fourthRow = [
-    [122, 120, 99, 118, 98, 110, 109, 44, 46, 47],
-    [90, 88, 67, 86, 66, 78, 77, 60, 62, 63]
-];
+class keybord {
+    textarea;
 
-(function getTextarea() {
-    const body = document.querySelector('body');
-    const textarea = document.createElement('textarea');
-    body.classList.add('container');
-    textarea.classList.add('textarea');
-    body.appendChild(textarea);
-})();
+    init(keyLayout) {
+        const insertLineBreak = ["9", "Shift", "Ctrl", "capslock"];
+        let keyElement;
+        this.wrapper = document.createElement('div');
+        this.wrapper.setAttribute('class', 'wrapper');
+        this.keyBlock = document.createElement('div');
+        this.keyBlock.setAttribute('class', 'keyboard');
+        this.keyBlock.setAttribute('id', 'keyboard');
+        this.textArea = document.createElement('textarea');
+        this.textArea.setAttribute('rows', '15');
+        this.wrapper.appendChild(this.textArea);
+        for (let i = 0; i < keyLayout.length; i++) {
+            keyElement = document.createElement('div');
+            keyElement.setAttribute('class', 'keys');
+            keyElement.setAttribute('id', keyLayout[i]);
+            switch (keyLayout[i]) {
+                case 8:
+                    keyLayout[i] = 'backspace';
+                    break;
+                case 9:
+                    keyLayout[i] = 'Tab';
+                    break;
+                case 13:
+                    keyLayout[i] = 'Enter';
+                    break;
+                case 16:
+                    keyLayout[i] = 'Shift';
+                    break;
+                case 17:
+                    keyLayout[i] = 'Ctrl';
+                    break;
+                case 18:
+                    keyLayout[i] = 'Alt';
+                    break;
+                case '32':
+                    keyLayout[i] = '       ';
+                    break;
+                case 20:
+                    keyLayout[i] = 'capslock';
+                    break;
+                default:
+                    break;
+            }
+            let specialCharacters = typeof keyLayout[i];
+            if (specialCharacters === 'string') {
+                keyElement.innerHTML = keyLayout[i];
+            } else {
+                keyElement.innerHTML = String.fromCharCode(keyLayout[i]);
+            }
+            for (let j = 0; j < insertLineBreak.length; j++) {
+                if (keyLayout[i] == insertLineBreak[j]) {
+                    this.keyBlock.innerHTML += '<br>';
+                }
+            }
+            this.keyBlock.appendChild(keyElement);
+            this.wrapper.appendChild(this.keyBlock);
+        }
+        return this.wrapper;
+    }
+}
 
-(function generateKeyboard() {
-    const keyCodeName = [
-        ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace"],
-        ["Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash", "Delete"],
-        ["CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter"],
-        ["ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ArrowUp", "ShiftRight"],
-        ["ControlLeft", "MetaLeft", "AltLeft", "Space", "AltRight", "ArrowLeft", "ArrowDown", "ArrowRight", "ControlRight"]
-    ];
-    const sysKeys = ['Backspace', 'Tab', 'Del', 'CapsLock', 'Enter', 'Shift', '&#x25B2;', 'Shift', 'Ctrl', 'Win', 'Alt', 'Alt', '&#x25C4;', '&#x25BC;', '&#x25BA;', 'Ctrl'];
-    const keyboard = document.createElement('div');
-    const body = document.querySelector('body');
-    keyboard.classList.add('keyboard');
-    body.appendChild(keyboard);
+const keyLayoutEn = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, 9,
+    113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 20, 97, 115, 100,
+    102, 103, 104, 106, 107, 108, 59, 39, 13, 16, 122, 120, 99, 118, 98, 110, 109,
+    44, 46, 47, 92, 17, 18, 32, 'ru', 'en'];
+const keyLayoutRu = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, 9,
+    1081, 1094, 1091, 1082, 1077, 1085, 1075, 1096, 1097, 1079, 91, 93, 20, 1092, 1099, 1074,
+    1072, 1087, 1088, 1086, 1083, 1076, 59, 39, 13, 16, 1103, 1095, 1089, 1084, 1080, 1090, 1100,
+    44, 46, 47, 92, 17, 18, 32, 'ru', 'en'];
+let output;
+let virtualKeyboard = new keybord();
+let storageLanguage = localStorage.getItem('language');
+if (storageLanguage === 'ru') {
+    output = virtualKeyboard.init(keyLayoutRu);
+} else {
+    output = virtualKeyboard.init(keyLayoutEn);
+}
+document.body.appendChild(output);
+document.onkeypress = function (events) {
+    console.log(event.code);
+    console.log(event.keyCode);
+    document.getElementById(event.keyCode).classList.add('active');
+}
+document.querySelectorAll('.keys').forEach(function (element) {
+    element.onclick = function (event) {
+        let code = this.getAttribute('id');
+        this.classList.add('active');
+        let insertTextarea = document.getElementsByTagName('textarea');
+        virtualKeyboard.textArea.innerHTML += String.fromCharCode(code);
+    }
+})
+document.getElementById('ru').onclick = function (event) {
+    localStorage.setItem("language", 'ru');
+    document.getElementById('ru').classList.add('active');
+    output = virtualKeyboard.init(keyLayoutRu);
+}
+document.getElementById('en').onclick = function (event) {
+    localStorage.setItem("language", 'en');
+    document.getElementById('en').classList.add('active');
+    output = virtualKeyboard.init(keyLayoutEn);
+}
